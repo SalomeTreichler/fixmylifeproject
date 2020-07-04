dd8a98f8cb35304ac328df77ad93a0abdb32f3ab
Salome Treichler <salome.treichler@noseryoung.ch>
2020-07-04 15:05:59 +0200

    added tomato logo to bottom

--- fixmylife/src/App.js
+++ fixmylife/src/App.js
@@ -1,35 +1,45 @@
-import React from "react";
-import "./App.css";
+import React, { Fragment } from "react";
 import useStyle from "./AppStyle";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import logo from "./images/FMLLogo.png";
 import Grid from "@material-ui/core/Grid";
+import tomatoLogo from "./images/tomatoBagLogo.png"
 
 function App() {
   const classes = useStyle();
 
   return (
-    <AppBar elevation={0} position="static">
-      <Toolbar>
-        <img src={logo} alt="FML logo" className={classes.logo} />
-        <Grid
-          container
-          direction="row"
-          justify="center"
-          alignItems="center"
-          className={classes.grid}
+    <Fragment>
+      <AppBar elevation={0} position="static">
+        <Toolbar>
+          <img src={logo} alt="FML logo" className={classes.logo} />
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            className={classes.grid}
           >
-          <Typography
-            variant="h2"
-            className={classes.applicationTitle}
-          >
-            Fix my Life
+            <Typography
+              variant="h2"
+              className={classes.applicationTitle}
+            >
+              Fix my Life
           </Typography>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <Grid container direction="row" justify="flex-end" alignItems="flex-end" spacing={1} className={classes.footer}>
+        <Grid item>
+          <Typography>Powered by</Typography>
+          </Grid>
+        <Grid item>
+          <img src={tomatoLogo} alt="Tomato Bag Logo" className={classes.tomatoLogo}/>
         </Grid>
-      </Toolbar>
-    </AppBar>
+      </Grid>
+    </Fragment>
   );
 }
 
--- fixmylife/src/AppStyle.js
+++ fixmylife/src/AppStyle.js
@@ -10,4 +10,13 @@ export default makeStyles((theme) => ({
   grid: {
     position: "absolute",
   },
+  footer: {
+    position: "fixed",
+    bottom: 0,
+    paddingRight: 10,
+    paddingBottom: 10
+  },
+  tomatoLogo:{
+    width: 30,
+  }
 }));
